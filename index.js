453847e1a5de1fa478c6005512b8fd0c72145b38
Richard Tabares <richardtabaresb@gmail.com>
2025-08-27 16:45:11 -0500

    feat: Implementacion de AuthWithPassword

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
-import { supabase } from './supabaseClient.js'
+import { supabase } from './services/supabaseClient.js'
 
 //habilita variables de entorno
 dotenv.config()
@@ -15,91 +15,120 @@ app.use(express.json())
 const PORT = process.env.PORT
 
 //funcion para crear manualmente el idLink
-const idGeneraror = () => (Math.random().toString(36).substring(2, 8))
+const idGeneraror = () => Math.random().toString(36).substring(2, 8)
 
 //endopoint para crear el id del link y devolver un objeto con la informacion completa
 app.post('/createIdLink', async (req, res) => {
+	//se recibe la url del body para acortarla
+	const { inputUrl } = req.body
+	let idLink = ''
+	//varibale utilizada para validar que se cree un idLink unico
+	//en la base de datos
+	let exits = true
 
-    //se recibe la url del body para acortarla
-    const { inputUrl } = req.body
-    let idLink = ''
-    //varibale utilizada para validar que se cree un idLink unico
-    //en la base de datos
-    let exits = true
+	//se obtiene protocolo y host para URL base
+	const baseUrl = `${req.protocol}://${req.get('host')}`
 
-    //se obtiene protocolo y host para URL base
-    const baseUrl = (`${req.protocol}://${req.get("host")}`)
+	try {
+		//antes de guardar debo verificar si ya existe un idLink
+		while (exits) {
+			idLink = idGeneraror()
 
-    try {
-        
-        //antes de guardar debo verificar si ya existe un idLink
-        while (exits) {
+			//consult a la DB si existe el idLink generado
+			const { data: existingLink, error: existingError } = await supabase
+				.from('links')
+				.select()
+				.eq('idLink', idLink)
+				.maybeSingle()
 
-            idLink = idGeneraror()
+			//lanzamos error si hay algun problema con la coneccion a la DB
+			if (existingError)
+				throw new Error('Error al conectarse con el servicio')
 
-            //consult a la DB si existe el idLink generado
-            const { data: existingLink, error: existingError } = await supabase
-            .from('links')
-            .select()
-            .eq('idLink', idLink)
-            .maybeSingle()
-            
-            //lanzamos error si hay algun problema con la coneccion a la DB
-            if (existingError) throw new Error('Error al conectarse con el servicio')
-            
-            //validamos que si la consulta haya devuelto informacion
-            //en caso de devolver, podra un false
-            //en caso de no devolver nada, podra true y seguira el while
-            // hasta encontrar un idLink unico
-            exits = !!existingLink
-            
-        }
+			//validamos que si la consulta haya devuelto informacion
+			//en caso de devolver, pondra un false
+			//en caso de no devolver nada, podra true y seguira el while
+			// hasta encontrar un idLink unico
+			exits = !!existingLink
+		}
 
-        //se crea objeto para unir ambas partes, url base e idLink
-        const shortLink = {
-            idLink,
-            url: inputUrl,
-            shortLink: `${baseUrl}/${idLink}`
-        };
+		//se crea objeto para unir ambas partes, url base e idLink
+		const shortLink = {
+			idLink,
+			url: inputUrl,
+			shortLink: `${baseUrl}/${idLink}`,
+		}
 
-        //guardar en supabasee
-        const { error } = await supabase
-            .from('links')
-            .insert([shortLink])
+		//guardar en supabasee
+		const { error } = await supabase.from('links').insert([shortLink])
 
-        //lanzamos error si hay algun problema con la coneccion a la DB
-        if (error) throw new Error('Error al conectarse con el servicio')
+		//lanzamos error si hay algun problema con la coneccion a la DB
+		if (error) throw new Error('Error al conectarse con el servicio')
 
-        //enviadmo objeto json al frontend
-        res.send(shortLink)
-
-    } catch (error) {
-
-        //retornamos el error de ser asi
-        return res.status(500).json({ error: error.message })
-
-    }
-
+		//enviamos objeto json al frontend con la informacion
+		res.send(shortLink)
+	} catch (error) {
+		//retornamos el error de ser asi
+		return res.status(500).json({ error: error.message })
+	}
 })
 
 //api para consultar el idLink y redireccion a la url original
 app.get('/:idLink', async (req, res) => {
+	//adicionar try para asegurarnos la consulta
+	const idLink = req.params.idLink
 
+	try {
+		const { data, error } = await supabase
+			.from('links')
+			.select()
+			.eq('idLink', idLink)
+			.single()
 
-    //adicionar try para asegurarnos la consulta
-    const idLink = req.params.idLink
-
-    const { data, error } = await supabase
-        .from('links')
-        .select()
-        .eq('idLink', idLink)
-        .single()
-
-    return res.redirect(data.url)
-
+		if (error) throw new Error('Error al conectarse con el servicio')
+		return res.redirect(data.url)
+	} catch (error) {
+		return res.status(500).json({ error: error.message })
+	}
 })
 
+app.post('/login', async (req, res) => {
+	//extraemos usuario y contraseña del body
+	const { user, pass } = req.body
+	try {
+		const { data, error } = await supabase.auth.signInWithPassword({
+			email: user,
+			password: pass,
+		})
+		//lanzamos error si hay algun problema con la coneccion a la DB
+		if (error) throw new Error(error)
+		//enviamos objeto json al frontend con la informacion
+		res.send(data)
+	} catch (error) {
+		//retornamos el error de ser asi
+		return res.status(500).json({ error: error.message })
+	}
+})
+app.post('/singup', async (req, res) => {
+	//extraemos usuario y contraseña del body
+	const { user, pass } = req.body
+
+	try {
+		const { data, error } = await supabase.auth.signUp({
+			email: user,
+			password: pass,
+		})
+		//lanzamos error si hay algun problema con la coneccion a la DB
+		if (error) throw new Error(error)
+		//enviamos objeto json al frontend con la informacion
+		console.log(error)
+		res.send(data)
+	} catch (error) {
+		//retornamos el error de ser asi
+		return res.status(500).json({ error: error.message })
+	}
+})
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+	console.log(`Server is running on port ${PORT}`)
 })
